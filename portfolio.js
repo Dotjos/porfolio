1db209fcba5c8a028d8e7f4469540ef328600a3e
Dotjos <oladotjos@gmail.com>
2023-08-21 18:40:39 +0100

    Mobile nav

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+// function fill(dat) {
+//   dat.style.fill = "green";
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const tSvg = document.querySelector(".tSvg");
+//   tSvg.addEventListener();
+// });
