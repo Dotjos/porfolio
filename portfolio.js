96c47b6783de6990d3a9062340cd1db3f95e4e70
Dotjos <oladotjos@gmail.com>
2023-08-22 00:47:55 +0100

    dark mode

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,15 @@
-// function fill(dat) {
-//   dat.style.fill = "green";
-// }
+function modeToggle() {}
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const tSvg = document.querySelector(".tSvg");
-//   tSvg.addEventListener();
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector(".toggleBtn");
+  const navBar = document.querySelectorAll(".navB");
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("bg-slate-900");
+    document.body.classList.toggle("text-white");
+
+    navBar.forEach((div) => {
+      div.classList.toggle("bg-slate-800");
+      div.classList.toggle("bg-slate-100");
+    });
+  });
+});
