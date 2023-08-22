fe89397f73d5f23bb6e3717e7453a32b7ff50156
Dotjos <oladotjos@gmail.com>
2023-08-22 01:50:51 +0100

    mobile view

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,17 @@
-function modeToggle() {}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleBtn = document.querySelector(".toggleBtn");
-  const navBar = document.querySelectorAll(".navB");
-  toggleBtn.addEventListener("click", () => {
+function modeToggle(btn, navBar) {
+  btn.addEventListener("click", () => {
     document.body.classList.toggle("bg-slate-900");
     document.body.classList.toggle("text-white");
-
     navBar.forEach((div) => {
       div.classList.toggle("bg-slate-800");
       div.classList.toggle("bg-slate-100");
     });
   });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector(".toggleBtn");
+  const navBar = document.querySelectorAll(".navB");
+
+  modeToggle(toggleBtn, navBar);
 });
